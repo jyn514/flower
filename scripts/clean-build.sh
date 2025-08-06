dfe0f9e7374692957a0b9fbc3c13cafd9cb6c2c0
jyn <github@jyn.dev>
2025-08-06 13:41:03 -0400

    make it clear that clean-build supports graal

--- scripts/clean-build.sh
+++ scripts/clean-build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-# only meant for quick iteration times in dev; for real sites use graal
 set -e
 
 PATH=~/src/flower/target:$PATH
 export PATH
 
 cd ~/src/flower
-clojure -T:build uberjar
+# only meant for quick iteration times in dev; for real sites use graal
+# clojure -T:build uberjar
+clojure -T:build native
 cd ..
 rm -rf flower-test
 mkdir flower-test
