7d0d6bf4d3957d8dcbea26482426107c0f53b325
jyn <github@jyn.dev>
2025-08-16 01:25:13 -0400

    wip: move template embedding to a transformer

--- defaults/transformers/embed.clj
+++ defaults/transformers/embed.clj
@@ -1,21 +1,7 @@
-(use 'hiccup2.core 'flower.utils 'flower.reflect 'flower.expressions.html)
-
-(def ^:dynamic root)
-
-(defn transform [{page :content}]
-  ; replace-with! modifies in place, so we need to hold on to the root node
-  (binding [root (document page)]
-    (loop [tags (select root "flower-embed")]
-      (when (seq tags)
-        (doseq [tag tags]
-          (let [embedded (html tag)
-                attrs (attrs tag)
-                template-name (get attrs "template")
-                template (flower.reflect/template template-name)
-                locals {(symbol (get attrs "name")) embedded}
-                rendered (flower.reflect/render template template-name locals)]
-            (if (is-root rendered)
-              (set! root rendered)
-              (replace-with! tag rendered))))
-        (recur (select root "flower-embed"))))
-      root))
+(use 'flower.utils 'expressions.meta)
+(defn transform [{page :content meta :frontmatter}]
+  (let [template (:template meta "default.html")]
+    (println template)
+    (if (some? template)
+      (embed template {'content page})
+      page)))
