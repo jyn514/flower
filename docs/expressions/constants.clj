aa775968e10f33278e81658f2c336cd42896ac03
jyn <github@jyn.dev>
2025-10-18 20:21:21 -0400

    add description to docs

--- docs/expressions/constants.clj
+++ docs/expressions/constants.clj
@@ -2,5 +2,5 @@
 
 (def site-title "flower documentation")
 (def site-author "jyn")
-(def global-desc site-title)
+(def global-desc "flower is a static site generator that grows with you")
 (def lang "en")
