7aedd2aeb78060899d041c7a533379dde42480e0
jyn <github@jyn.dev>
2025-12-07 15:16:24 -0500

    add test that ninja rebuilds when an implicit dep is modified

--- test/process/clean_build.clj
+++ test/process/clean_build.clj
@@ -1,6 +1,8 @@
 (ns test.process.clean-build
   (:require
    [babashka.fs :as fs]
+   [clj-commons.digest :as digest]
+   [clojure.string :as str]
    [clojure.test :refer [use-fixtures]]
    [expectations.clojure.test :refer [defexpect expect]]
    [test.helpers :refer [build-assert-no-rebuild exit-success flower!
@@ -9,6 +11,7 @@
 (use-fixtures :once once-fixture)
 
 (defn tmpdir [] (str (fs/create-temp-dir {:prefix "flower-clean-build-"})))
+(defn / [dir & args] (str (apply fs/path dir args)))
 
 (defn build-new []
   (let [dir (tmpdir)]
@@ -33,3 +36,27 @@
     (spit (str (fs/path dir "pages" "my-new-post.md")) "this is some *markdown*!")
     (expect exit-success (build-assert-no-rebuild dir))
     (expect (fs/exists? (fs/path dir "public" "my-new-post.html")))))
+
+(defn has-contents [page str]
+  (str/includes? (String. (fs/read-all-bytes page)) str))
+
+; unfortunately this doesn't thing we actually care about, which is that `flower watch` notices it needs to rebuild :/
+(defexpect depfile-modified []
+  (let [site (build-new)
+        other-dir (tmpdir)
+        dynamic-dep (/ other-dir "README.md")
+        in (/ site "pages" "hello.md")
+        out (/ site "public" "hello.html")
+        x (apply str (repeat 25 "x"))
+        y (apply str (repeat 25 "y"))]
+    (println site dynamic-dep)
+    ; Add a file that has an implicit dependency somewhere we don't know about.
+    (spit dynamic-dep x)
+    (spit in (str "◊" '(require '[expressions.meta :refer [include]])
+                  "◊" `(~'include ~dynamic-dep)))
+    (expect exit-success (build-assert-no-rebuild site))
+    (expect (has-contents out x))
+    ; Modify the dependency and make sure the site gets rebuilt.
+    (spit dynamic-dep y)
+    (expect exit-success (build-assert-no-rebuild site))
+    (expect (has-contents out y))))
