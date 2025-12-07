d9f4729f29f6fe5d155f7fa4f80d1ed5bf314887
jyn <github@jyn.dev>
2025-12-07 08:08:45 -0500

    make jyn_dev test more useful

--- test/process/jyn_dev.clj
+++ test/process/jyn_dev.clj
@@ -1,15 +1,18 @@
 (ns test.process.jyn-dev
   (:require
+   [babashka.fs :as fs]
    [clojure.test :refer [use-fixtures]]
    [expectations.clojure.test :refer [defexpect expect]]
-   [test.helpers :refer [once-fixture flower-cli flower! build-assert-no-rebuild]]))
+   [test.helpers :refer [build-assert-no-rebuild once-fixture]]))
 
 (use-fixtures :once once-fixture)
 
 (defexpect build-jyn-dev []
+  (fs/delete-tree "blossom/.build")
   (let [{:keys [exit]} (build-assert-no-rebuild "blossom" {:rebuild-flower false})]
     (expect 0 exit)))
 
-(defexpect build-jyn-dev []
+(defexpect build-docs []
+  (fs/delete-tree "docs/.build")
   (let [{:keys [exit]} (build-assert-no-rebuild "docs")]
     (expect 0 exit)))
